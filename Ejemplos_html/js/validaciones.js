113e331aeebd73c88a05ed412bb6eee1d4a04b11
capmedina01 <kalichecomfenalco@gmail.com>
2022-10-05 13:42:18 -0500

    validacion

---
+++ Ejemplos_html/js/validaciones.js
@@ -0,0 +1,7 @@
+function validarFormulario(){
+  var username=document.registroUser.username;
+  var correo=document.registroUser.correo;
+  var passw= document.registroUser.passw;
+
+  alert(username)
+}
